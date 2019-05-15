c1ff337d456ed750286db01b3b0ee32980f6cc84
Josh <joshcho@Joshs-MacBook-Pro.local>
2019-05-15 15:24:59 -0700

    Implement React Modal and add open and close functionalities for DetailView

--- react-client/src/components/Vehicle.jsx
+++ react-client/src/components/Vehicle.jsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Modal from 'react-modal';
+import DetailView from './DetailView.jsx';
 
-const Vehicle = ({ vehicle }) => (
-  <div>
-    <div className="vehicle-wrapper">
-      <div className="vehicle-detail">
-        {vehicle.Year}
+Modal.setAppElement('#app');
+
+class Vehicle extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalIsOpen: false,
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState(prevState => ({
+      modalIsOpen: !prevState.modalIsOpen,
+    }));
+  }
+
+  render() {
+    const { modalIsOpen } = this.state;
+    const { vehicle } = this.props;
+    return (
+      <div>
+        <div className="vehicle-wrapper" onClick={this.toggleModal}>
+          <div className="vehicle-detail">
+            {vehicle.Year}
+          </div>
+
+          <div className="vehicle-detail">
+            {vehicle.Make}
+          </div>
+          <div className="vehicle-detail">
+            {vehicle.Model}
+          </div>
+        </div>
+        {modalIsOpen && (
+          <Modal isOpen={modalIsOpen} onRequestClose={this.toggleModal} contentLabel="Detail View">
+            <DetailView vehicle={vehicle} />
+          </Modal>
+        )}
       </div>
-      <div className="vehicle-detail">
-        {vehicle.Make}
-      </div>
-      <div className="vehicle-detail">
-        {vehicle.Model}
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Vehicle;
