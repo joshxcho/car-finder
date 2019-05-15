fce12e25404c5094ea817f6637442341fccd38a6
joshxcho <joshcho@Joshs-MacBook-Pro.local>
2019-05-15 16:13:41 -0700

    Set up Jest Enzyme

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  setupTestFrameworkScriptFile: '<rootDir>/react-client/src/setupTests.js',
+  testEnvironment: 'node',
+};
